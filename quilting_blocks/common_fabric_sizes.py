55c054b7c7b59af87da43cf00be9b7261b642478
Maria D'Angelo <dangelo.maria@gmail.com>
2024-04-14 14:05:59 -0600

    Sorted common fabric sizes by square inches and fixed some errors

--- quilting_blocks/common_fabric_sizes.py
+++ quilting_blocks/common_fabric_sizes.py
@@ -4,12 +4,13 @@ fabric_size = namedtuple(
     "fabric_size", ["name", "length", "width", "square_inches"])
 
 FABRIC_CUTS = {
-    "fat_quarter": fabric_size("fat_quarter", 18, 21, 378),
-    "fat_eighth": fabric_size("fat_eight", 9, 21, 189),
-    "yard": fabric_size("yard", 36, 42, 1512),
-    "fat_half": fabric_size("fat_half", 18, 42, 756),
     "fat_sixteenth": fabric_size("fat_sixteenth", 4.5, 21, 94.5),
+    "sixteenth_yard": fabric_size("sixteenth_yard", 2.25, 42, 94.5),
+    "fat_eighth": fabric_size("fat_eight", 9, 21, 189),
+    "eighth_yard": fabric_size("eighth_yard", 4.5, 42, 189),
+    "fat_quarter": fabric_size("fat_quarter", 18, 21, 378),
+    "quarter_yard": fabric_size("quarter_yard", 9, 42, 378),
+    "fat_half": fabric_size("fat_half", 21, 36, 756),
     "half_yard": fabric_size("half_yard", 18, 42, 756),
-    "quarter_yard": fabric_size("quarter_yard", 9, 21, 189),
-    "eighth_yard": fabric_size("eighth_yard", 4.5, 21, 94.5),
+    "yard": fabric_size("yard", 36, 42, 1512),
 }
